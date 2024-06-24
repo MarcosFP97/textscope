dde9ad5592e149b4ab503ebe4a122c421c73f688
MarcosFP97 <marcosfernandez.pichel@usc.es>
2024-06-24 13:59:30 +0200

    Validated relevance filter method

--- textscope/config.py
+++ textscope/config.py
@@ -1,5 +1,5 @@
 # Configuración y definiciones de temas y subtemas
-GAMBLING_THEMES = ["ludopatía", "juego patológico", "adicción al juego"]
+GAMBLING_THEMES = ["ludopatía", "trastorno de juego", "adicción al juego", "trastorno por juego", "trastorno de juego adictivo", "trastorno por juego adictivo", "juego compulsivo", "dependencia al juego"]
 GAMBLING_SUBTHEMES = {
     "technology": ["AI", "programming", "gadgets"],
     "health": ["nutrition", "exercise", "mental health"],
--- textscope/relevance_analyzer.py
+++ textscope/relevance_analyzer.py
@@ -15,7 +15,7 @@ class RelevanceAnalyzer:
         self.model = AutoModel.from_pretrained(model_name)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
-        self.task = 'Given a search query, determine whether the text is relevant for the query'
+        self.task = 'Given a query, determine whether the text is relevant for the query'
     
     def __get_detailed_instruct(
         self,
@@ -31,7 +31,7 @@ class RelevanceAnalyzer:
     ) -> Tensor:
         last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
         return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
-
+    
     def analyze(
             self, 
             text:str
@@ -40,22 +40,21 @@ class RelevanceAnalyzer:
             return 0.
         
         # Lógica para analizar la relevancia del texto con respecto a una serie de queries que modelan la temática
-        rels = []
-        for k in self.themes:
-            instruct = [
-                self.__get_detailed_instruct(self.task, k),
-            ]
-            documents = [
-                text
-            ]
-            input = instruct + documents
-            batch_dict = self.tokenizer(input, max_length=512, padding=True, truncation=True, return_tensors='pt')
-            batch_dict = {k: v.to(self.device) for k, v in batch_dict.items()}
-            outputs = self.model(**batch_dict)
-            embeddings = self.__average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
-            # normalize embeddings
-            embeddings = F.normalize(embeddings, p=2, dim=1)
-            scores = (embeddings[:1] @ embeddings[1:].T) * 100
-            rels.append(scores.tolist()[0][0])
-
-        return np.mean(rels)
+        concat_q = ' '.join(self.themes)
+        instruct = [
+            self.__get_detailed_instruct(self.task, concat_q),
+        ]
+        doc = [
+            text
+        ]
+        input = instruct + doc
+        batch_dict = self.tokenizer(input, max_length=512, padding=True, truncation=True, return_tensors='pt')
+        batch_dict = {k: v.to(self.device) for k, v in batch_dict.items()}
+        outputs = self.model(**batch_dict)
+        embeddings = self.__average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+        # normalize embeddings
+        embeddings = F.normalize(embeddings, p=2, dim=1)
+        scores = (embeddings[:1] @ embeddings[1:].T) * 100
+        return scores.tolist()[0][0]
+    
+   
