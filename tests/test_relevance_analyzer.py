4e37f54691e9fd8006539b339ed8db4c884fb3ff
MarcosFP97 <marcosfernandez.pichel@usc.es>
2026-09-17 10:15:08 +0200

    Gender violence profile added + test on Galician support

--- tests/test_relevance_analyzer.py
+++ tests/test_relevance_analyzer.py
@@ -1,7 +1,7 @@
 import pytest
 from textscope.relevance_analyzer import RelevanceAnalyzer
 
-sample_text = 'La adicción al juego es una enfermedad, pero es la única enfermedad que te puede hacer rico. La artritis no te va a hacer ganar un centavo'
+sample_text = 'A prevención da violencia de xénero require detectar situacións de control, maltrato psicolóxico e agresións dentro da parella. A protección das vítimas e a educación en igualdade son fundamentais para combater a violencia machista.'
 sample_text_rel_small = 'El acceso a internet debe ser reconocido como un derecho humano básico en la era digital.'
 sample_text_rel_large = 'Los derechos digitales son fundamentales para garantizar que las personas puedan expresarse libremente y acceder a información en línea sin temor a censura. Además, es crucial proteger la privacidad de los datos personales frente a corporaciones y gobiernos, asegurando así la seguridad en la navegación y la comunicación digital.'
 
@@ -10,7 +10,7 @@ def analyzer():
     return RelevanceAnalyzer()
 
 def test_analyze_relevance(analyzer):
-    score = analyzer.analyze(sample_text, "gambling")
+    score = analyzer.analyze(sample_text, "gender_violence_gal")
     print(f"Relevance score for sample text: {score}")
     assert isinstance(score, float), "The result should be a float"
     assert 0 <= score <= 100, "The result should be between 0 and 100"
